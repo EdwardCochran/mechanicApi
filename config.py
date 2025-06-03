acf34be37e4e5839fa6a9c86d29f2f4f8a03fe9f
Edward Cochran <djdoubbled@gmail.com>
2025-06-03 15:24:59 -0400

    added project dependencies

--- config.py
+++
@@ -1,9 +0,0 @@
-class DevelopmentConfig:
-    sqlalchemy_database_uri = 'mysql+mysqlconnector://root:power317@localhost/mechanic_db'
-    DEBUG = True
-    
-class TestingConfig:
-    pass
-    
-class ProductionConfig:
-    pass    
